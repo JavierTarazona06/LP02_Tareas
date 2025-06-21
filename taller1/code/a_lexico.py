d0cf8b5e485f5345384e76049f3f2c08e14076be
Javier Tarazona <96489034+JavierTarazona06@users.noreply.github.com>
2025-06-21 01:10:31 -0500

    LexAnalyzer

--- taller1/code/a_lexico.py
+++ taller1/code/a_lexico.py
@@ -1,76 +1,196 @@
+import re
 import ply.lex as lex
 
-# 1. Lista de tokens
-tokens = ()
-
-# ------------------------------------
-# ------------------------------------
-
-# 3-4. Reglas regex simples -  Ejecuciones por tipo
-
+# Lista de tokens
+tokens = []
 
 # -------------------------------
 #   PALABCLAVE
 # ------------------------------
 
+tokens.append('PALABCLAVE')
+
+palabras_reservadas = [
+    'Func','Principal','imprimir','Vacio','Retornar','Global',
+    'Romper','Continuar','Pasar','Sino',
+    'Si','Entonces','Para','Mientras','Lanzar','Intentar',
+    'Excepto'
+]
+reserved = { w: 'PALABCLAVE' for w in palabras_reservadas }
 
 # -------------------------------
 #   TIPO
 # ------------------------------
 
+tokens += ['TIPOA', 'TIPOB']
+
+tiposa = ['Bool','Entero','Flotante','Cadena','Caracter']
+reserved.update({ w: 'TIPOA' for w in tiposa})
+
+tiposb = ['Conjunto','Arreglo','Matriz','MatrizRachas','Multicotomizacion','M2VClasificacion','Diccionario']
+'''for t in tiposa:
+    tiposb.append(f'Conjunto<{t}>')
+    tiposb.append(f'Arreglo<{t}>')
+    tiposb.append(f'Matriz<{t}>')
+    tiposb.append(f'MatrizRachas<{t}>')
+    tiposb.append(f'Multicotomizacion<{t}>')
+    tiposb.append(f'M2VClasificacion<{t}>')
+    tiposb.append(f'Diccionario<{t},{t}>')'''
+reserved.update({ w: 'TIPOB' for w in tiposb })
+
+#TIPOB_PATTERN = '|'.join(re.escape(s) for s in tiposb)
+#TIPOB_PATTERN = fr'(?:{TIPOB_PATTERN})'
+
+'''def t_TIPOB(t):
+    t.type = 'TIPOB'
+    return t
+t_TIPOB.__doc__ = TIPOB_PATTERN'''
+
+# -------------------------------
+#   Caracter y Cadenas
+# ------------------------------
+# TODO: No reconoce la cadena f
+tokens += ['CARACTER', 'CADENA']
+
+t_CARACTER = r"'(?:\\.|[^\\'])'"
+
+CADENA_S = r'"(?:\\.|[^"\\])*"'
+CADENA_F = (
+    r'"(?:'
+    r'\\.'
+    r'|[^"\\{]'
+    r'|\{[A-Za-z_][A-Za-z0-9_]*\}'
+    r')*"'
+)
+CADENA_PATTERN = rf'(?:{CADENA_S}|{CADENA_F})'
+
+def t_CADENA(t):
+    return t
+t_CADENA.__doc__ = CADENA_PATTERN
 
 # -------------------------------
 #   ID
 # ------------------------------
 
+tokens += ['ID', 'BOOL']
 
+reserved.update({ b: 'BOOL' for b in ['Verdadero','Falso'] })
+
+def t_ID(t):
+    r'[A-Za-z_][A-Za-z0-9_]*'
+    t.type = reserved.get(t.value, 'ID')
+    return t
 
 # -------------------------------
 #   LITERAL
 # ------------------------------
 
+tokens += ['ENTERO','REAL','COMPLEJO']
 
+DIGITO = r'[0-9]'
+NATURAL = rf'(?:[1-9]{DIGITO}*|0)'
+ENTERO_PATTERN = rf'(?:-?{NATURAL})'
+REAL_U = rf'(?:{NATURAL}?\.{NATURAL})(?:[eE][+-]?{NATURAL})?'
+REAL_PATTERN = rf'(?:-?{REAL_U})'
+COMPLEJO_PATTERN = rf'(?:{REAL_PATTERN}[+-]{REAL_U}[jJ]|{REAL_PATTERN}[jJ])'
 
-# -------------------------------
-#   OPERADOR
-# ------------------------------
+def t_COMPLEJO(t):
+    t.value = complex(t.value.replace('J','j'))
+    return t
 
+t_COMPLEJO.__doc__ = COMPLEJO_PATTERN
 
+def t_REAL(t):
+    t.value = float(t.value)
+    return t
 
-# -------------------------------
-#   DELIM
-# ------------------------------
+t_REAL.__doc__ = REAL_PATTERN
 
+def t_ENTERO(t):
+    t.value = int(t.value)
+    return t
 
-
-
+t_ENTERO.__doc__ = ENTERO_PATTERN
 
 # -------------------------------
 #   COMMENT
 # ------------------------------
 
+tokens += ["COMMENT"]
 
+LINEA = r'//[^\n]*'
+BLOQUECO = r'/\*[\s\S]*?\*/'
+COMMENT_PATTERN = rf'(?:{LINEA}|{BLOQUECO})'
+
+def t_COMMENT(t):
+    return t
+
+t_COMMENT.__doc__ = COMMENT_PATTERN
+
+# -------------------------------
+#   OPERADOR
+# ------------------------------
+
+tokens += ['OPREL', 'OPASI', 'OPASIU', 'OPACC', 'OPARIT', 'OPLOG']
+
+t_OPREL = r'(?:<=|>=|==|!=|<|>)'
+t_OPASI = r'(?:\*\*=|//=|\+=|-=|\*=|/=|%=|@=|=)'
+t_OPASIU = r'(?:\+\+|--)'
+t_OPACC = r'[\[\]\.]'
+t_OPARIT = r'(?:\*\*|//|\+|\-|\*|/|%|@)'
+t_OPLOG = r'(?:&&|\|\||NO|Y|O|!)'
+
+# -------------------------------
+#   DELIM
+# ------------------------------
+
+tokens += ["DELIM"]
+
+t_DELIM = r'[;{},()]'
 
 
 # ------------------------------------
 # ------------------------------------
 
-# 4. Ignorar espacios y tabulaciones
+# Espacios y Saltos de Línea
 t_ignore = ' \t'
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
 
-# 5. Error
+#tokens = tuple(tokens)
+
+# Error
 def t_error(t):
     print(f"Error: carácter inesperado '{t.value[0]}'")
     t.lexer.skip(1)
 
-# 6. Construir lexer
-lexer = lex.lex()
+# Construir lexer
+lexer = lex.lex(reflags=lex.re.VERBOSE)
 
 if __name__ == "__main__":
+    
     while True:
+        s = input('calc> ')
+        if not s:
+            break
+        with open(s, 'r', encoding='utf-8') as f:
+            datos = f.read()
+        lexer.input(datos)
+
+        with open(s[:-4]+'_tokens.txt', 'w', encoding='utf-8') as fout:
+            while True:
+                tok = lexer.token()
+                if not tok:
+                    break
+                # tok.type es el nombre del token, tok.value su lexema,
+                # tok.lineno y tok.lexpos la línea y posición
+                fout.write(f"{tok.lineno}:{tok.lexpos}\t{tok.type}\t{tok.value}\n")
+
+    '''while True:
         s = input('calc> ')
         if not s:
             break
         lexer.input(s)
         for tok in lexer:
-            print(tok)
+            print(tok)'''
